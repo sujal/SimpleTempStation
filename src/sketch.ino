aac57e6e683229bd4592925825551d6741437416
sujal shah <codesujal@gmail.com>
2012-12-25 00:38:22 -0500

    fix compile, setup for BLE event loop

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,7 +1,7 @@
-# File: sketch.ino
-# Description: A simple remote temp station, interrogatable over BLE
-# Copyright: 2012 Forche, LLC.
-# Author: Sujal Shah
+// File: sketch.ino
+// Description: A simple remote temp station, interrogatable over BLE
+// Copyright: 2012 Forche, LLC.
+// Author: Sujal Shah
 
 #include <Boards.h>
 #include "Wire.h"
@@ -36,7 +36,16 @@ Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 float readingsHistory[MAX_HISTORY];
 int mostRecentIndex = MAX_HISTORY-1;
 
+boolean FIRST_CHECK = true;
+
+unsigned long lastCheckMillis;
+unsigned long pollingInterval;
+
 void setup() {
+
+  pollingInterval = POLLING_INTERVAL_SEC * 1000;
+  lastCheckMillis = 0ul;
+
   Serial.begin(9600);
   Serial.print("MAX_HISTORY IS ");
   Serial.println(MAX_HISTORY);
@@ -49,108 +58,118 @@ float lastTemp = 0.0;
 
 void loop() {
 
-    float temp = bmp.readTemperature();
+    unsigned long currentMillis = millis();
 
-    float ftemp = (temp * (9.0/5.0)) + 32.0f;
+    // Serial.println(currentMillis);
 
-    if (ftemp != readingsHistory[mostRecentIndex]) {
-      digitalWrite(LED_PIN_OUT, HIGH);
-    } else {
-      digitalWrite(LED_PIN_OUT, LOW);
+    if (FIRST_CHECK || (currentMillis - lastCheckMillis) > pollingInterval) {
+      FIRST_CHECK=false;
+      lastCheckMillis = currentMillis;
+      float temp = bmp.readTemperature();
+
+      float ftemp = (temp * (9.0/5.0)) + 32.0f;
+
+      if (ftemp != readingsHistory[mostRecentIndex]) {
+        digitalWrite(LED_PIN_OUT, HIGH);
+      } else {
+        digitalWrite(LED_PIN_OUT, LOW);
+      }
+      mostRecentIndex++;
+      if (mostRecentIndex >= MAX_HISTORY) {
+        mostRecentIndex = 0;
+      }
+
+      Serial.print("storing in ");
+      Serial.println(mostRecentIndex);
+
+      readingsHistory[mostRecentIndex] = ftemp;
+
+      Serial.print("Temperature = ");
+      Serial.print(temp);
+      Serial.print(" *C / ");
+      Serial.print(ftemp);
+      Serial.println(" *F");
+
+      Serial.print("Pressure = ");
+      Serial.print(bmp.readPressure());
+      Serial.println(" Pa");
+
+      // Calculate altitude assuming 'standard' barometric
+      // pressure of 1013.25 millibar = 101325 Pascal
+      Serial.print("Altitude = ");
+      Serial.print(bmp.readAltitude(101422));
+      Serial.println(" meters");
+
+      Serial.println();
+
+      matrix.clear();
+      int j = mostRecentIndex-(MATRIX_WIDTH-1);   // 0 - 8 = -8 -> 288 -8 = 280; index
+      if (j < 0) {
+        j = MAX_HISTORY + j;
+      }
+
+      for (int i=0; i<MATRIX_WIDTH; i++) {
+
+        int orange = 0;
+        int red = 0;
+        int green = 0;
+
+        int index = j+i;
+        if (index >= MAX_HISTORY) {
+          index-=MAX_HISTORY;
+        }
+
+        float thisFtemp = readingsHistory[index];
+
+        green = (int)floor((thisFtemp-10.0f)/10.0f);
+
+        Serial.println(green);
+
+        if (green > MATRIX_HEIGHT) {
+          green = MATRIX_HEIGHT;
+        }
+
+        if (green < 0) {
+          green = 0;
+        }
+
+        if (green > GREEN_HEIGHT+ORANGE_HEIGHT) {
+          red = green - (GREEN_HEIGHT+ORANGE_HEIGHT);
+          green -= red;
+        }
+
+        if (green > GREEN_HEIGHT) {
+          orange = green - GREEN_HEIGHT;
+          green -= orange;
+        }
+
+        int startPos = MATRIX_HEIGHT-1;
+        int endPoint = startPos - green + 1;
+
+        if (green > 0) {
+          matrix.drawLine(i,startPos,i,endPoint,LED_GREEN);
+
+        }
+
+        startPos = endPoint-1;
+
+        if (orange > 0) {
+          endPoint -= orange;
+          matrix.drawLine(i,startPos, i, endPoint, LED_YELLOW);
+        }
+
+        startPos = endPoint-1;
+
+        if (red > 0) {
+          endPoint -= red;
+          matrix.drawLine(i,startPos, i, endPoint, LED_RED);
+        }
+
+      }
+      matrix.writeDisplay();
+
     }
-    mostRecentIndex++;
-    if (mostRecentIndex >= MAX_HISTORY) {
-      mostRecentIndex = 0;
-    }
 
-    Serial.print("storing in ");
-    Serial.println(mostRecentIndex);
 
-    readingsHistory[mostRecentIndex] = ftemp;
-
-    Serial.print("Temperature = ");
-    Serial.print(temp);
-    Serial.print(" *C / ");
-    Serial.print(ftemp);
-    Serial.println(" *F");
-
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude(101422));
-    Serial.println(" meters");
-
-    Serial.println();
-
-    matrix.clear();
-    int j = mostRecentIndex-(MATRIX_WIDTH-1);   // 0 - 8 = -8 -> 288 -8 = 280; index
-    if (j < 0) {
-      j = MAX_HISTORY + j;
-    }
-
-    for (int i=0; i<MATRIX_WIDTH; i++) {
-
-      int orange = 0;
-      int red = 0;
-      int green = 0;
-
-      int index = j+i;
-      if (index >= MAX_HISTORY) {
-        index-=MAX_HISTORY;
-      }
-
-      float thisFtemp = readingsHistory[index];
-
-      green = (int)floor((thisFtemp-10.0f)/10.0f);
-
-      Serial.println(green);
-
-      if (green > MATRIX_HEIGHT) {
-        green = MATRIX_HEIGHT;
-      }
-
-      if (green < 0) {
-        green = 0;
-      }
-
-      if (green > GREEN_HEIGHT+ORANGE_HEIGHT) {
-        red = green - (GREEN_HEIGHT+ORANGE_HEIGHT);
-        green -= red;
-      }
-
-      if (green > GREEN_HEIGHT) {
-        orange = green - GREEN_HEIGHT;
-        green -= orange;
-      }
-
-      int startPos = MATRIX_HEIGHT-1;
-      int endPoint = startPos - green + 1;
-
-      if (green > 0) {
-        matrix.drawLine(i,startPos,i,endPoint,LED_GREEN);
-
-      }
-
-      startPos = endPoint-1;
-
-      if (orange > 0) {
-        endPoint -= orange;
-        matrix.drawLine(i,startPos, i, endPoint, LED_YELLOW);
-      }
-
-      startPos = endPoint-1;
-
-      if (red > 0) {
-        endPoint -= red;
-        matrix.drawLine(i,startPos, i, endPoint, LED_RED);
-      }
-
-    }
-    matrix.writeDisplay();
-
-    delay(300000);
+    // delay(POLLING_INTERVAL_SEC*1000);
 }
